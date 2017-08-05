d725a6ff9ac21b39878eb2e38c142e196d1d59f3
Gauchy Clement <clement.gauchy@student.ecp.fr>
2017-08-05 09:44:19 +0200

    Plotting the total spin of the system, using ggplot

---
+++ Spin.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(gganimate)
+
+#Setting of the principal parameters
+NUMBER = 50
+SPIN = 0.5
+TMAX = 500
+
+#Each particule has a uniform probabilaty that their spin equals 1/2 or -1/2
+#the matrix Status register all the microstates of the N particules at each row
+Status = matrix(0, nrow=TMAX, ncol=NUMBER)
+
+#Initially, all particules are in a + state
+Status[1,] = rep(SPIN,NUMBER)
+
+#Evaluate each step of the system
+for(i in 2:TMAX){
+  Status[i,] = (2*rbinom(NUMBER,1,0.5)-1)*SPIN
+}
+
+totalSpin = apply(Status,1,sum)
+Step = 1:TMAX
+time = 1:TMAX
+
+#Convert into a dataframe
+df = data.frame(Step,totalSpin)
+
+#using ggplot2
+
+ggplot(df, aes(x = Step, y = totalSpin, frame = time)) + geom_point()
